614d208a4e82a62b09fe92e687b45d2dee223179
thaismr <thaismr@users.noreply.github.com>
2021-01-08 15:17:58 -0300

    Add 'purchase' and 'purchase item' models.

--- purchases/admin.py
+++ purchases/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Purchase, PurchaseItem
+
+
+# Add inline form to include 'purchase item'
+class PurchaseItemInline(admin.TabularInline):
+    model = PurchaseItem
+    extra = 1
+
+
+# Show inline form at 'Purchase' admin
+class PurchaseAdmin(admin.ModelAdmin):
+    inlines = [
+        PurchaseItemInline,
+    ]
+
 
 # Register your models here.
+admin.site.register(Purchase, PurchaseAdmin)
+admin.site.register(PurchaseItem)
--- purchases/models.py
+++ purchases/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product, Variation
+
 
 # Create your models here.
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    total = models.FloatField(default=0)
+    status = models.CharField(
+        default='C',
+        max_length=1,
+        choices=(
+            ('A', 'Aprovado'),
+            ('C', 'Criado'),
+            ('E', 'Enviado'),
+            ('F', 'Finalizado'),
+            ('P', 'Pendente'),
+            ('R', 'Reprovado'),
+        )
+    )
+
+    def __str__(self):
+        return f'Cart #{self.pk}'
+
+
+class PurchaseItem(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation = models.ForeignKey(Variation, on_delete=models.CASCADE)
+    price = models.FloatField(default=0)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'Item #{self.pk}'
